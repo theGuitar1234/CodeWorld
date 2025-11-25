559424c874c306abdffc37755dd1dcd8b0a12002
The Guitar <turanlizad776@gmail.com>
2025-11-25 18:25:47 +0400

    massive changes has been made (trust me bro)

--- src/main/java/az/codeworld/springboot/web/controllers/IndexController.java
+++ src/main/java/az/codeworld/springboot/web/controllers/IndexController.java
@@ -2,6 +2,8 @@ package az.codeworld.springboot.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class IndexController {
@@ -9,6 +11,11 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/404")
+    public String notFound() {
+        return "error/404";
     }
     
 }
